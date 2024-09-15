b856c90df3914cd2d80342f36874ee9734a4dea1
Taiho1998 <tgo12323@gmail.com>
2024-09-15 11:52:47 +0900

    알고리즘 초안 작성

--- ReadTheCode/main.js
+++ ReadTheCode/main.js
@@ -8,6 +8,21 @@ function solution(code) {
     var answer = ''
     var mode = 0
     for (let i = 0; i < code.length; i++){
+        if (mode == 0){
+            if (code[i] != 1 && i%2 == 0){
+                answer += code[i]
+            }
+            else if (code[i] == 1){
+                mode = 1
+            }
+        }else {
+            if (code[i] != 1 && i%2 == 1){
+                answer += code[i]
+            }
+            else if (code[i] == 1){
+                mode = 0
+            }
+        }
     }
     return answer;
 }
