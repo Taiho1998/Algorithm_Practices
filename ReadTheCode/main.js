190725ce2b2ee5d2afc807b81e2e12d19ce7fdca
Taiho1998 <tgo12323@gmail.com>
2024-09-15 11:48:00 +0900

    기본 조건 작성

---
+++ ReadTheCode/main.js
@@ -0,0 +1,15 @@
+// 문제 출처 https://school.programmers.co.kr/learn/courses/30/lessons/181932
+
+"use strict";
+
+let str = "abc1abc1abc" //테스트 케이스
+
+function solution(code) {
+    var answer = ''
+    var mode = 0
+    for (let i = 0; i < code.length; i++){
+    }
+    return answer;
+}
+
+solution(str)
