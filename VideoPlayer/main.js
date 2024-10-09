f7e3b0c625d04669bd62431b1c01c4ed71af611a
Taiho1998 <tgo12323@gmail.com>
2024-10-09 12:30:22 +0900

    정수형 데이터를 다시 00:00 형태의 문자열로 변환하는 함수 생성

--- VideoPlayer/main.js
+++ VideoPlayer/main.js
@@ -9,12 +9,34 @@ let commands = 	["next", "prev"]
 function solution(video_len, pos, op_start, op_end, commands) {
     var answer = '';
     let videoLen = transToInt(video_len)
+    let posTime = transToInt(pos)
+    let opStart = transToInt(op_start)
+    let opEnd = transToInt(op_end)
+    console.log(transToTime(opEnd))
     return answer;
 }
 
+// 전달받은 시간 정보 정수로 변환
 function transToInt(str) {
     let arr = str.split(':')
     return parseInt(arr[1]) + (60 * parseInt(arr[0]))
+}
+
+function transToTime(int) {
+    let min = parseInt(int / 60)
+    let sec = parseInt(int % 60)
+    let strmin, strsec
+    if (min < 10){
+        strmin = '0' + min.toString()
+    } else {
+        strmin = min.toString()
+    }
+    if (sec < 10){
+        strsec = '0' + sec.toString()
+    } else {
+        strsec = sec.toString()
+    }
+    return `${strmin}:${strsec}`
 }
 
 solution(video_len, pos, op_start, op_end, commands)
