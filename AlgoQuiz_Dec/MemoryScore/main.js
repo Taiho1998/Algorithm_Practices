2a439fdc5d6c52d7b3270985f2d46eb0ef30d7bc
Taiho1998 <tgo12323@gmail.com>
2024-12-01 23:51:38 +0900

    추억 점수 문제 해결

--- AlgoQuiz_Dec/MemoryScore/main.js
+++ AlgoQuiz_Dec/MemoryScore/main.js
@@ -1,4 +1,23 @@
+var Aname = ["may", "kein", "kain", "radi"];
+var yearning = [5, 10, 1, 3];
+var photo = [
+  ["may", "kein", "kain", "radi"],
+  ["may", "kein", "brin", "deny"],
+  ["kon", "kain", "may", "coni"],
+];
+
 function solution(name, yearning, photo) {
   var answer = [];
+  for (let i = 0; i < photo.length; i++) {
+    let total = 0;
+    for (let j = 0; j < photo[i].length; j++) {
+      if (name.indexOf(photo[i][j]) !== -1) {
+        total += yearning[name.indexOf(photo[i][j])];
+      }
+    }
+    answer.push(total);
+  }
   return answer;
 }
+
+console.log(solution(Aname, yearning, photo));
